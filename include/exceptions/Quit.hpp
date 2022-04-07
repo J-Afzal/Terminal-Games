fe083e2b1fa162a846b1d8d2c437fb11abdf57a8
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-07 18:58:15 +0100

    move custom exception

---
+++ include/exceptions/Quit.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file Quit.hpp
+ * @author Junaid Afzal
+ * @brief A custom exception that allows for returning to
+ * the main menu when many functions deep in a call stack
+ * @version 1.0
+ * @date 29-03-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+namespace Exceptions
+{
+    class Quit : public std::exception {};
+}
