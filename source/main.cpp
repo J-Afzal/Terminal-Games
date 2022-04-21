4933931d43b3d8168e841eaf247b6146dc616b33
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-21 02:19:02 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Junaid Afzal
+ * @brief Executes the main menu
+ * @version 1.0
+ * @date 21-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include "helpers/MainMenu.hpp"
 
 int main()
