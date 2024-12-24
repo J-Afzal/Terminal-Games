49aa95e8d287f3662d9561c0df493b8a1daae1f4
Junaid Afzal <56676071+J-Afzal@users.noreply.github.com>
2024-12-24 16:32:54 +0000

    source linting fixes

--- source/games/TicTacToe.cpp
+++ source/games/TicTacToe.cpp
@@ -1,185 +1,203 @@
+#include <algorithm>
 #include <chrono>
 #include <cstdint>
 #include <string>
 #include <thread>
+#include <tuple>
 #include <vector>
 
 #include "games/TicTacToe.hpp"
+#include "helpers/Constants.hpp"
 #include "helpers/Exceptions.hpp"
 #include "helpers/PageBuilder.hpp"
 #include "helpers/Terminal.hpp"
 
-TicTacToe::TicTacToe(const bool& outputIsOnlyASCII)
+namespace TerminalGames
 {
-    m_pageBuilder.SetProperties(Pages::TICTACTOE, outputIsOnlyASCII);
-    m_randomNumberGenerator.seed(std::chrono::system_clock::now().time_since_epoch().count());
-}
-
-void TicTacToe::SetupGame()
-{
-    m_movesRemaining.clear();
-    m_randomNumberGenerator() % 2 == 0 ? m_currentPlayer = "Player X" : m_currentPlayer = "Player O";
-    m_playerCount = "N/A";
-    m_userPlayerChoice = " ";
-    m_AISpeedName = "N/A";
-    m_turnCount = 0;
-    m_hasWinner = false;
-
-    for (uint32_t i = 0, GridNumber = 0; i < 3; i++)
+    TicTacToe::TicTacToe(const bool &onlyUseASCII) // NOLINT(cert-msc32-c,cert-msc51-cpp,cppcoreguidelines-pro-type-member-init,hicpp-member-init)
     {
-        for (uint32_t j = 0; j < 3; j++, GridNumber++)
-        {
-            m_gameGrid[i][j] = ' ';
-            m_movesRemaining.push_back(GridNumber); // 0-8
-        }
-    }
-}
-
-void TicTacToe::UpdateGameInfo()
-{
-    m_gameInfo.ticTacToeStruct = { m_gameGrid, m_currentPlayer, m_playerCount, m_AISpeedName, m_turnCount, m_hasWinner };
-}
-
-void TicTacToe::GetUserOptions()
-{
-    GetPlayerCount();
-
-    // If only one human user, then ask them which player they want to be (X or O)
-    if (m_playerCount == "1  ")
-        GetUserPlayerChoice();
-
-    // If AI involved get AI difficulty and speed
-    if (m_playerCount != "2  ") // i.e. = 0 or = 1
-        GetAISpeed();
-}
-
-void TicTacToe::GetPlayerCount()
-{
-    std::vector<std::string> menus = m_pageBuilder.GetPlayerCountOptionSelectionGamePages(m_gameInfo);
-    m_playerCount = std::to_string(Terminal::GetUserChoiceFromGameMenus(menus)) + "  ";
-}
-
-void TicTacToe::GetUserPlayerChoice()
-{
-    std::vector<std::string> menus = m_pageBuilder.GetUserPlayerChoiceOptionSelectionGamePages(m_gameInfo);
-    Terminal::GetUserChoiceFromGameMenus(menus) == 0 ? m_userPlayerChoice = "Player X" : m_userPlayerChoice = "Player O";
-}
-
-void TicTacToe::GetAISpeed()
-{
-    std::vector<std::string> menus = m_pageBuilder.GetAISpeedOptionSelectionGamePages(m_gameInfo);
-    m_AISpeed = Terminal::GetUserChoiceFromGameMenus(menus);
-
-    if (m_AISpeed == 0)
-        m_AISpeedName = "INSTANT";
-    else if (m_AISpeed == 1)
-        m_AISpeedName = "FAST";
-    else // == 2
-        m_AISpeedName = "SLOW";
-}
-
-bool TicTacToe::IsGameOver()
-{
-    // Check horizontals
-    if ((m_gameGrid[0][0] != ' ' && m_gameGrid[0][0] == m_gameGrid[0][1] && m_gameGrid[0][1] == m_gameGrid[0][2]) ||
-        (m_gameGrid[1][0] != ' ' && m_gameGrid[1][0] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[1][2]) ||
-        (m_gameGrid[2][0] != ' ' && m_gameGrid[2][0] == m_gameGrid[2][1] && m_gameGrid[2][1] == m_gameGrid[2][2]) ||
-
-        // Check verticals
-        (m_gameGrid[0][0] != ' ' && m_gameGrid[0][0] == m_gameGrid[1][0] && m_gameGrid[1][0] == m_gameGrid[2][0]) ||
-        (m_gameGrid[0][1] != ' ' && m_gameGrid[0][1] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[2][1]) ||
-        (m_gameGrid[0][2] != ' ' && m_gameGrid[0][2] == m_gameGrid[1][2] && m_gameGrid[1][2] == m_gameGrid[2][2]) ||
-
-        // Check diagonals
-        (m_gameGrid[0][0] != ' ' && m_gameGrid[0][0] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[2][2]) ||
-        (m_gameGrid[2][0] != ' ' && m_gameGrid[2][0] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[0][2]))
-    {
-        m_hasWinner = true;
-        return m_hasWinner;
+        m_pageBuilder.SetProperties(Pages::TICTACTOE, onlyUseASCII);
+        m_randomNumberGenerator.seed(std::chrono::system_clock::now().time_since_epoch().count());
     }
 
-    else
-        return m_turnCount == 9;
-}
-
-void TicTacToe::ToggleCurrentPlayer()
-{
-    m_currentPlayer == "Player X" ? m_currentPlayer = "Player O" : m_currentPlayer = "Player X";
-}
-
-bool TicTacToe::IsCurrentTurnUsers()
-{
-    // If it is two player game then next turn will always be the user's turn, if not then
-    // check if the current player is the same as the user's choice. For a zero player game
-    // m_UserPlayerChoice = ' ' and so this will always return false.
-    return m_playerCount == "2  " || m_currentPlayer == m_userPlayerChoice;
-}
-
-void TicTacToe::ExecuteUserCommand()
-{
-    Terminal::PrintOutput(m_pageBuilder.GetUserCommandPage(m_gameInfo));
-
-    Terminal::SetCursorVisibility(true);
-
-    uint32_t KeyPress, Row = m_movesRemaining[0] / 3, Column = m_movesRemaining[0] % 3;
-
-    while (true)
+    void TicTacToe::SetupGame()
     {
-        while (true)
+        m_movesRemaining.clear();
+        m_randomNumberGenerator() % 2 == 0 ? m_currentPlayer = "Player X" : m_currentPlayer = "Player O";
+        m_playerCount = "N/A";
+        m_userPlayerChoice = " ";
+        m_AISpeedName = "N/A";
+        m_turnCount = 0;
+        m_hasWinner = false;
+
+        for (uint32_t i = 0; i < g_TICTACTOE_BOARD_HEIGHT; i++)
         {
-            Terminal::SetCursorPosition(3 + Column * 4, 4 + Row * 2);
-
-            KeyPress = Terminal::GetNextKeyPress();
-
-            if (KeyPress == '\r') // enter key
-                break;
-            else if (KeyPress == 72) // up arrow key
-                Row == 0 ? Row = 2 : --Row;
-            else if (KeyPress == 80) // down arrow key
-                Row == 2 ? Row = 0 : ++Row;
-            else if (KeyPress == 75) // left arrow key
-                Column == 0 ? Column = 2 : --Column;
-            else if (KeyPress == 77) // right arrow key
-                Column == 2 ? Column = 0 : ++Column;
-            else if (KeyPress == 'q')
+            for (uint32_t j = 0; j < g_TICTACTOE_BOARD_WIDTH; j++)
             {
-                Terminal::SetCursorVisibility(false);
-                throw Exceptions::QuitGame();
+                m_gameGrid[i][j] = ' '; // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
+                m_movesRemaining.emplace_back(i, j);
             }
         }
+    }
 
-        auto CommandPosition = std::find(m_movesRemaining.begin(), m_movesRemaining.end(), Row * 3 + Column);
+    void TicTacToe::UpdateGameInfo()
+    {
+        m_gameInfo.ticTacToeStruct = {
+            .gameGrid=m_gameGrid,
+            .currentPlayer=m_currentPlayer,
+            .playerCount=m_playerCount,
+            .AISpeedName=m_AISpeedName,
+            .turnCount=m_turnCount,
+            .hasWinner=m_hasWinner
+        };
+    }
 
-        if (CommandPosition != m_movesRemaining.end())
+    void TicTacToe::GetUserOptions()
+    {
+        GetPlayerCount();
+
+        // If only one human user, then ask them which player they want to be (X or O)
+        if (m_playerCount == "1  ")
         {
-            m_gameGrid[Row][Column] = m_currentPlayer.back();
-            m_movesRemaining.erase(CommandPosition);
-            m_turnCount++;
-            Terminal::SetCursorVisibility(false);
+            GetUserPlayerChoice();
+        }
+
+        // If AI involved get AI difficulty and speed
+        if (m_playerCount != "2  ") // i.e. = 0 or = 1
+        {
+            GetAISpeed();
+        }
+    }
+
+    void TicTacToe::GetPlayerCount()
+    {
+        const std::vector<std::string> menus = m_pageBuilder.GetPlayerCountOptionSelectionGamePages(m_gameInfo);
+        m_playerCount = std::to_string(Terminal::GetUserChoiceFromGameMenus(menus)) + "  ";
+    }
+
+    void TicTacToe::GetUserPlayerChoice()
+    {
+        const std::vector<std::string> menus = m_pageBuilder.GetUserPlayerChoiceOptionSelectionGamePages(m_gameInfo);
+        Terminal::GetUserChoiceFromGameMenus(menus) == 0 ? m_userPlayerChoice = "Player X" : m_userPlayerChoice = "Player O";
+    }
+
+    void TicTacToe::GetAISpeed()
+    {
+        const std::vector<std::string> menus = m_pageBuilder.GetAISpeedOptionSelectionGamePages(m_gameInfo);
+        m_AISpeed = Terminal::GetUserChoiceFromGameMenus(menus);
+
+        if (m_AISpeed == 0)
+        {
+            m_AISpeedName = "INSTANT";
+        }
+
+        else if (m_AISpeed == 1)
+        {
+            m_AISpeedName = "FAST";
+        }
+
+        else // == 2
+        {
+            m_AISpeedName = "SLOW";
+        }
+    }
+
+    bool TicTacToe::IsGameOver()
+    {
+        // Check horizontals
+        if ((m_gameGrid[0][0] != ' ' && m_gameGrid[0][0] == m_gameGrid[0][1] && m_gameGrid[0][1] == m_gameGrid[0][2]) ||
+            (m_gameGrid[1][0] != ' ' && m_gameGrid[1][0] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[1][2]) ||
+            (m_gameGrid[2][0] != ' ' && m_gameGrid[2][0] == m_gameGrid[2][1] && m_gameGrid[2][1] == m_gameGrid[2][2]) ||
+
+            // Check verticals
+            (m_gameGrid[0][0] != ' ' && m_gameGrid[0][0] == m_gameGrid[1][0] && m_gameGrid[1][0] == m_gameGrid[2][0]) ||
+            (m_gameGrid[0][1] != ' ' && m_gameGrid[0][1] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[2][1]) ||
+            (m_gameGrid[0][2] != ' ' && m_gameGrid[0][2] == m_gameGrid[1][2] && m_gameGrid[1][2] == m_gameGrid[2][2]) ||
+
+            // Check diagonals
+            (m_gameGrid[0][0] != ' ' && m_gameGrid[0][0] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[2][2]) ||
+            (m_gameGrid[2][0] != ' ' && m_gameGrid[2][0] == m_gameGrid[1][1] && m_gameGrid[1][1] == m_gameGrid[0][2]))
+        {
+            m_hasWinner = true;
+            return m_hasWinner;
+        }
+
+        return m_turnCount == g_TICTACTOE_MAXIMUM_ERROR_COUNT;
+    }
+
+    void TicTacToe::ToggleCurrentPlayer()
+    {
+        m_currentPlayer == "Player X" ? m_currentPlayer = "Player O" : m_currentPlayer = "Player X";
+    }
+
+    bool TicTacToe::IsCurrentTurnUsers()
+    {
+        // If it is two player game then next turn will always be the user's turn, if not then
+        // check if the current player is the same as the user's choice. For a zero player game
+        // m_UserPlayerChoice = ' ' and so this will always return false.
+        return m_playerCount == "2  " || m_currentPlayer == m_userPlayerChoice;
+    }
+
+    void TicTacToe::ExecuteUserCommand()
+    {
+        Terminal::PrintOutput(m_pageBuilder.GetUserCommandPage(m_gameInfo));
+
+        const uint32_t startingRow = std::get<0>(m_movesRemaining.front());
+        const uint32_t startingColumn = std::get<1>(m_movesRemaining.front());
+
+        while (true)
+        {
+            const std::tuple<uint32_t, uint32_t> selectedCommand = Terminal::GetUserCommandFromGameGrid(startingRow, startingColumn, g_TICTACTOE_BOARD_HEIGHT - 1, g_TICTACTOE_BOARD_WIDTH - 1, g_TICTACTOE_GRID_LEFT_PAD, g_TICTACTOE_GRID_TOP_PAD, g_TICTACTOE_GRID_ELEMENT_WIDTH, g_TICTACTOE_GRID_ELEMENT_HEIGHT);
+
+            if (ValidateCommand(selectedCommand))
+            {
+                ExecuteGeneralCommand(selectedCommand);
+                return;
+            }
+        }
+    }
+
+    void TicTacToe::ExecuteAICommand()
+    {
+        if (m_AISpeed != 0)
+        {
+            Terminal::PrintOutput(m_pageBuilder.GetAICommandPage(m_gameInfo));
+            std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(m_AISpeed));
+        }
+
+        const std::tuple<uint32_t, uint32_t> selectedCommand = m_movesRemaining[m_randomNumberGenerator() % m_movesRemaining.size()];
+
+        if (ValidateCommand(selectedCommand))
+        {
+            ExecuteGeneralCommand(selectedCommand);
             return;
         }
     }
-}
 
-void TicTacToe::ExecuteAICommand()
-{
-    if (m_AISpeed != 0)
+    bool TicTacToe::ValidateCommand(const std::tuple<uint32_t, uint32_t>& command)
     {
-        Terminal::PrintOutput(m_pageBuilder.GetAICommandPage(m_gameInfo));
-        std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(m_AISpeed));
+        const auto commandPosition = std::ranges::find(m_movesRemaining.begin(), m_movesRemaining.end(), command, {});
+
+        return commandPosition != m_movesRemaining.end();
     }
 
-    m_AICommand = m_movesRemaining[m_randomNumberGenerator() % m_movesRemaining.size()];
+    void TicTacToe::ExecuteGeneralCommand(const std::tuple<uint32_t, uint32_t>& command)
+    {
+        const auto commandPosition = std::ranges::find(m_movesRemaining.begin(), m_movesRemaining.end(), command, {});
 
-    m_gameGrid[m_AICommand / 3][m_AICommand % 3] = m_currentPlayer.back();
-    m_movesRemaining.erase(std::find(m_movesRemaining.begin(), m_movesRemaining.end(), m_AICommand));
-    m_turnCount++;
-}
+        m_gameGrid[std::get<0>(command)][std::get<1>(command)] = m_currentPlayer.back(); // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
+        m_movesRemaining.erase(commandPosition);
+        m_turnCount++;
+        Terminal::SetCursorVisibility(false);
+    }
 
-void TicTacToe::GameOver()
-{
-    Terminal::PrintOutput(m_pageBuilder.GetGameOverPage(m_gameInfo));
+    void TicTacToe::GameOver()
+    {
+        Terminal::PrintOutput(m_pageBuilder.GetGameOverPage(m_gameInfo));
 
-    if (Terminal::GetNextKeyPress() == 'q')
-        throw Exceptions::QuitGame();
-}
+        if (Terminal::GetNextKeyPress() == 'q')
+        {
+            throw Exceptions::QuitGame();
+        }
+    }
+} // namespace TerminalGames
--- source/helpers/MainMenu.cpp
+++ source/helpers/MainMenu.cpp
@@ -12,56 +12,71 @@
 #include "helpers/PageBuilder.hpp"
 #include "helpers/Terminal.hpp"
 
-MainMenu::MainMenu(int argc, char* argv[])
+namespace TerminalGames
 {
-    m_outputIsOnlyASCII = ParseCommandLineArguments(std::vector<std::string>(argv, argv + argc));
-}
+    MainMenu::MainMenu(const std::vector<std::string> &commandLineArguments) : m_onlyUseASCII(ParseCommandLineArguments(commandLineArguments)) {}
 
-MainMenu::MainMenu(const bool& outputIsOnlyASCII)
-{
-    m_outputIsOnlyASCII = outputIsOnlyASCII;
-}
+    MainMenu::MainMenu(const bool &onlyUseASCII) : m_onlyUseASCII(onlyUseASCII) {}
 
-bool MainMenu::ParseCommandLineArguments(const std::vector<std::string>& cliArgs) const
-{
-    if (cliArgs.size() == 1) // Default if no CLI args given.
-        return false;
-
-    if (cliArgs.size() == 2 && (cliArgs[0] == "--a" || cliArgs[0] == "--ascii-only"))
-        return true;
-
-    std::cout << "\nUsage: Terminal-Games [--a --ascii-only]\n\nOptions:\n\t--a --ascii-only\tOnly use ASCII characters." << std::endl;
-    exit(1);
-}
-
-MainMenu::~MainMenu()
-{
-    Terminal::Clear();
-    Terminal::SetCursorVisibility(true);
-    Terminal::SetCursorPosition(0, 0);
-}
-
-void MainMenu::Run()
-{
-    PageBuilder PageBuilder(Pages::MAINMENU, m_outputIsOnlyASCII);
-    m_mainMenus = PageBuilder.GetGameSelectionMainMenuPages({ "Tic Tac Toe", "Hangman", "Battleships" });
-
-    // The index of games in m_Games should match the index of the string in m_mainMenus which has the game selected,
-    // which is defined by the order of the input array to the previous line.
-    m_games.push_back(std::make_unique<TicTacToe>(m_outputIsOnlyASCII));
-    m_games.push_back(std::make_unique<Hangman>(m_outputIsOnlyASCII));
-    m_games.push_back(std::make_unique<Battleships>(m_outputIsOnlyASCII));
-
-    Terminal::SetCursorVisibility(false);
-    Terminal::SetCursorPosition(0, 0);
-
-    // Custom exceptions are used to return exit the games and the main menu.
-    while (true)
+    bool MainMenu::ParseCommandLineArguments(const std::vector<std::string>& commandLineArguments)
     {
-        try { m_games[Terminal::GetUserChoiceFromMainMenus(m_mainMenus)]->Play(); }
+        if (commandLineArguments.size() == 1) // Default if no CLI args given.
+        {
+            return false;
+        }
 
-        catch (Exceptions::QuitMainMenu& e) { return; }
+        if (commandLineArguments.size() == 2 && (commandLineArguments[0] == "--a" || commandLineArguments[0] == "--ascii-only"))
+        {
+            return true;
+        }
 
-        catch (Exceptions::QuitGame& e) { continue; }
+        std::cout << "\nUsage: Terminal-Games [--a --ascii-only]\n\nOptions:\n\t--a --ascii-only\tOnly use ASCII characters.\n";
+        exit(1); // NOLINT(concurrency-mt-unsafe)
     }
-}
+
+    MainMenu::~MainMenu()
+    {
+        Terminal::Clear();
+        Terminal::SetCursorVisibility(true);
+        Terminal::SetCursorPosition(0, 0);
+    }
+
+    void MainMenu::Run()
+    {
+        const PageBuilder PageBuilder(Pages::MAINMENU, m_onlyUseASCII);
+        const std::vector<std::string> menus({"Tic Tac Toe", "Hangman", "Battleships"}); // NOLINT(fuchsia-default-arguments-calls)
+        m_mainMenus = PageBuilder.GetGameSelectionMainMenuPages(menus);
+
+        // The index of games in m_Games should match the index of the string in m_mainMenus which has the game selected.
+        // This is defined by the order of the input array to the previous line.
+        m_games.push_back(std::make_unique<TicTacToe>(m_onlyUseASCII));
+        m_games.push_back(std::make_unique<Hangman>(m_onlyUseASCII));
+        m_games.push_back(std::make_unique<Battleships>(m_onlyUseASCII));
+
+        Terminal::SetCursorVisibility(false);
+        Terminal::SetCursorPosition(0, 0);
+
+        // Custom exceptions are used to return exit the games and the main menu.
+        while (true)
+        {
+            try
+            {
+                m_games[Terminal::GetUserChoiceFromMainMenus(m_mainMenus)]->Play();
+            }
+
+            catch (Exceptions::QuitMainMenu &e)
+            {
+                return;
+            }
+
+            catch (Exceptions::QuitGame &e)
+            {
+                continue;
+            }
+        }
+
+        Terminal::Clear();
+        Terminal::SetCursorVisibility(true);
+        Terminal::SetCursorPosition(0, 0);
+    }
+} // namespace TerminalGames
