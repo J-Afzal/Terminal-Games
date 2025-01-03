ff727d7e37a38883c7d27f57a93bac3c9d4f73b6
Junaid Afzal <56676071+J-Afzal@users.noreply.github.com>
2025-01-03 18:03:47 +0000

    add helpers and dependencies modules

---
+++ scripts/Dependencies.psm1
@@ -0,0 +1,127 @@
+$ErrorActionPreference = "Stop"
+
+#TODO: how to call from other module?
+
+<#
+    .SYNOPSIS
+    TODO
+
+    .DESCRIPTION
+    TODO
+
+    .INPUTS
+    TODO
+
+    .OUTPUTS
+    TODO
+
+    .EXAMPLE
+    TODO
+#>
+
+function Install-BuildDependencies {
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('macos-latest','ubuntu-latest','windows-latest')]
+        [String]
+        $OperatingSystem,
+    )
+
+    Write-Output "##[section]Running Install-BuildDependencies..."
+
+    Write-Verbose "##[debug]OperatingSystem: $OperatingSystem"
+
+    switch ($OperatingSystem) {
+        macos-latest {
+            Invoke-Expression "brew install ninja"
+        }
+
+        ubuntu-latest {
+            Invoke-Expression "sudo apt-get install ninja-build"
+        }
+
+        windows-latest {
+            Invoke-Expression "choco install ninja -y"
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
+    TODO
+
+    .DESCRIPTION
+    TODO
+
+    .INPUTS
+    TODO
+
+    .OUTPUTS
+    TODO
+
+    .EXAMPLE
+    TODO
+#>
+
+function Install-LintingDependencies {
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('macos-latest','ubuntu-latest','windows-latest')]
+        [String]
+        $OperatingSystem,
+    )
+
+    Write-Output "##[section]Running Install-LintingDependencies..."
+
+    Write-Verbose "##[debug]OperatingSystem: $OperatingSystem"
+
+    Write-Output "##[section]Installing npm dependencies..."
+
+    npm install
+
+    if ($LASTEXITCODE -eq 1) {
+        Write-Error "Please resolve the above errors!"
+    }
+
+    Write-Verbose "##[debug]Finished installing npm dependencies."
+
+    Write-Output "##[section]Installing clang-tidy and clang-format..."
+
+    switch ($OperatingSystem) {
+        macos-latest {
+            # Override pre-installed clang and add to path
+            Invoke-Expression "brew install llvm"
+            Invoke-Expression "export PATH='/opt/homebrew/opt/llvm/bin:$PATH'"
+        }
+
+        ubuntu-latest {
+            # Install brew to get latest llvm and clang tools
+            Invoke-Expression "test -d ~/.linuxbrew && eval '$(~/.linuxbrew/bin/brew shellenv)'"
+            Invoke-Expression "test -d /home/linuxbrew/.linuxbrew && eval '$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)'"
+            Invoke-Expression "echo 'eval \"\$($(brew --prefix)/bin/brew shellenv)\"' >> ~/.bashrc"
+            Invoke-Expression "export PATH='$HOME/.linuxbrew/bin:$PATH'"
+
+            Invoke-Expression "brew install llvm"
+        }
+
+        windows-latest {
+            Invoke-Expression "choco upgrade llvm -y"
+        }
+    }
+
+    Write-Verbose "##[debug]Finished installing clang-tidy and clang-format."
+
+    if (-Not Test-Path "./build/compile_commands.json") {
+
+        Write-Output "##[section]Configuring CMake to generate the 'compile_commands.json' file..."
+
+        # This will fail if ninja is not installed. Use Install-BuildDependencies to resolve this error.
+        cmake -S . -B ./build -G "Ninja"
+
+        Write-Verbose "##[debug]Finished configuring CMake to generate the 'compile_commands.json' file."
+    }
+}
---
+++ scripts/Helpers.psm1
@@ -0,0 +1,66 @@
+$ErrorActionPreference = "Stop"
+
+#TODO: how to call from other module?
+
+<#
+    .SYNOPSIS
+    TODO
+
+    .DESCRIPTION
+    TODO
+
+    .INPUTS
+    TODO
+
+    .OUTPUTS
+    TODO
+
+    .EXAMPLE
+    Import-Module Linters.psm1
+    Compare-ObjectExact -ReferenceObject $arrayOne -DifferenceObject $arrayTwo -Verbose
+#>
+
+function Compare-ObjectExact {
+    # TODO: document and add write-outputs, verbose, etc..
+
+    [CmdletBinding()]
+    [OutputType([System.Object[]])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [System.Object[]]
+        $ReferenceObject,
+
+        [Parameter(Mandatory = $true)]
+        [System.Object[]]
+        $DifferenceObject
+    )
+
+    [Collections.Generic.List[String]] $errors = @()
+
+    for ($index = 0; $index -lt $ReferenceObject.Length; $index++) {
+
+        try {
+
+            if ($ReferenceObject[$index] -ne $DifferenceObject[$index]) {
+                $errors.Add("'$($DifferenceObject[$index])' found instead of '$($ReferenceObject[$index])'.")
+            }
+        }
+
+        catch {
+
+            # Assuming that this is caused by an index out of bounds error with DifferenceObject
+            $errors.Add("'$($ReferenceObject[$index])' was not found.")
+
+        }
+    }
+
+    if ($DifferenceObject.Length -gt $ReferenceObject.Length) {
+
+        for ($index = $ReferenceObject.Length; $index -lt $DifferenceObject.Length; $index++) {
+
+            $errors.Add("An extra value of '$($DifferenceObject[$index])' found.")
+        }
+    }
+
+    return $errors
+}
