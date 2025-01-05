9e35e3b131d9535e5fe6c034ff77f2e743a2e7df
Junaid Afzal <56676071+J-Afzal@users.noreply.github.com>
2025-01-05 20:01:37 +0000

    Update Linters.psm1

--- modules/linters/Linters.psm1
+++ modules/linters/Linters.psm1
@@ -118,7 +118,7 @@ function Test-CodeUsingClang {
         switch ($Platform) {
             macos-latest {
                 brew install llvm
-                export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+                & bash "export PATH='/opt/homebrew/opt/llvm/bin:$PATH'"
                 $clangTidy = "clang-tidy"
                 $clangFormat = "clang-format"
             }
